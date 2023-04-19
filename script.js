09884812b75766b6aef3b7ca76ecfcd39f5b6baf
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-19 08:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+if (!navigator.userAgent.includes('Chrome') && navigator.userAgent.includes('Safari')) {
+    console.log('Browser is Safari');
+} else {
+    console.log('Browser is not Safari');
+}
